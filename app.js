4efcbba168a162fcf2295114418c5ea7df048e66
Jonathan0607 <jonathjve.43@gmail.com>
2025-06-18 15:10:11 -0500

    integrated sound

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ function darkModeToggle() {
         document.body.setAttribute('id', 'dark-mode');
         document.querySelector('.main-button').setAttribute('id', 'dark-mode');
     }
+    document.getElementById('click').currentTime = 0;
+    document.getElementById('click').play();
+
 }
 
 document.getElementsByTagName('button')[0].addEventListener('click', darkModeToggle);
